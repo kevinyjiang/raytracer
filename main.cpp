818d483de15f5e67c5a4932fcd38487a8e66e342
Kevin Jiang <kj.kevin@gmail.com>
2022-07-04 07:22:26 -0400

    refactor main to use camera class, and to use random doubles to implement sampling/basic anti aliasing

--- main.cpp
+++ main.cpp
@@ -3,36 +3,9 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
-
-// double hit_sphere(const point3& center, double radius, const ray& r) {
-//     vec3 oc = r.origin() - center;
-//     auto a = r.direction().length_squared();
-//     auto half_b = dot(oc, r.direction());
-//     auto c = oc.length_squared() - radius*radius;
-//     auto discriminant = half_b*half_b - a*c;
-//     if (discriminant < 0) {
-//         return -1.0;
-//     } else {
-//         return (-half_b - sqrt(discriminant)) / a;
-//     }
-// }
-
-// color ray_color(const ray& r) {
-//     auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-
-//     // Return color mapping of normal vectors from sphere on collision.
-//     if (t > 0.0) {
-//         vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-//         return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
-//     }
-//     vec3 unit_direction = unit_vector(r.direction());
-//     t = 0.5*(unit_direction.y() + 1.0);
-
-//     // If no collision, return gradiant. Linear interpolation between two color vectors.
-//     return (1.0-t)*color(1.0, 0, 1.0) + t*color(0.0, 1.0, 1.0);
-// }
 
 color ray_color(const ray& r, const hittable& world) {
     hit_record rec;
@@ -50,6 +23,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -58,14 +32,7 @@ int main() {
 
     // Camera
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
 
@@ -74,11 +41,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = double(i + random_double()) / (image_width-1);
+                auto v = double(j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
