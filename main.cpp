e42a0029fc1db8b9b910b9a7006c0dbbe608d01a
Kevin Jiang <kj.kevin@gmail.com>
2022-07-04 01:54:27 -0400

    color mapping to visualize sphere surface normals, also refactor hit_sphere

--- main.cpp
+++ main.cpp
@@ -4,22 +4,31 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 1, 1);
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
 
-    // Linear interpolation between two color vectors.
+    // Return color mapping of normal vectors from sphere on collision.
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5*(unit_direction.y() + 1.0);
+
+    // If no collision, return gradiant. Linear interpolation between two color vectors.
     return (1.0-t)*color(1.0, 0, 1.0) + t*color(0.0, 1.0, 1.0);
 }
 
