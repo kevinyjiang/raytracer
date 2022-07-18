873732597d2ad146457fe758fa20f0363fa7906c
Kevin Jiang <kj.kevin@gmail.com>
2022-07-18 19:40:07 -0400

    add cli arg to specify image width

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@
 #include "scenes.h"
 #include "color.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::srand(std::time(0));
     
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 300;
+    const int image_width = strtol(argv[1], nullptr, 0);
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
