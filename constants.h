3e9e9873438fa704f554d10ba89769aebdb4a278
Kevin Jiang <kj.kevin@gmail.com>
2022-07-18 21:42:43 -0400

    typo

--- constants.h
+++ constants.h
@@ -4,18 +4,12 @@
 #include <limits>
 #include <memory>
 
-#include "vec3.h"
-#include "ray.h"
-
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
-
-const color metals[2] = { color(.83, .7, .36), color(.01, .01, .01) };
-const color shiny[2] = { color(.95, .95, .95), color(.08, .08, .08) };
 
 // Utility Functions
 
@@ -41,3 +35,9 @@ inline int random_int(int min, int max) {
     // Returns a random integer in [min,max].
     return static_cast<int>(random_double(min, max+1));
 }
+
+#include "vec3.h"
+#include "ray.h"
+
+const color metals[2] = { color(.83, .7, .36), color(.01, .01, .01) };
+const color shiny[2] = { color(.95, .95, .95), color(.08, .08, .08) };
